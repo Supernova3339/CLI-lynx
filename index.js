9b14b951ddf9eef647946ce8cce5a88a1891c2a2
SuperDev <supernova@supers0ft.us>
2023-09-03 03:24:35 -0500

    Update API key and enhance link management functionality
    Updated the API key in configuration file (config.json) to maintain the secured connection with the server. Also, significant changes have been made to (index.js) to enhance link management functionality. New commands for creating, updating and deleting links have been added. These improvements provide a more robust and easy-to-use interface for managing links, thereby improving the user experience.

--- index.js
+++ index.js
@@ -134,7 +134,175 @@ program
 
 /*
 |----------------------------------------------------------------------------------
-| s
+| Link Commands
+|----------------------------------------------------------------------------------
+ */
+
+program
+    .command('link <action>')
+    .description('Manage links')
+    .action((action) => {
+        if (action === 'create') {
+            console.log('Creating a new link...');
+        } else if (action === 'update') {
+            console.log('Updating a link...');
+        } else if (action === 'delete') {
+            console.log('Deleting a link...');
+        } else {
+            console.error('Invalid action. Use "create", "update", or "delete".');
+        }
+    });
+
+// Create a link
+program
+    .command('create <slug> <destination>')
+    .description('Create a new link')
+    .requiredOption('-a, --author <author>', 'Author ID')
+    .action(async (slug, destination, options) => {
+        if (slug && destination) {
+            const apiUrl = config.apiUrl; // Assuming you have already configured the API URL
+            const secret = config.apiKey; // Assuming you have the API key in config.apiKey
+
+            const headers = {
+                'secret': secret, // Set the header name for authorization
+                'Authorization': config.apiKey, // Set the API Key from the configuration file
+            };
+
+            const data = {
+                slug,
+                destination,
+                author: options.author,
+            };
+
+            try {
+                const response = await axios.post(`${apiUrl}/link`, data, { headers });
+                if (response.status === 200) {
+                    const linkInfo = response.data.result;
+                    console.log('Link Successfully Created:');
+                    console.log(`ID: ${linkInfo.id}`);
+                    console.log(`Slug: ${linkInfo.slug}`);
+                    console.log(`Destination: ${linkInfo.destination}`);
+                    console.log(`Author: ${linkInfo.author}`);
+                    console.log(`Creation Date: ${linkInfo.creationDate}`);
+                    console.log(`Modified Date: ${linkInfo.modifiedDate}`);
+                    console.log(`Visits: ${linkInfo.visits}`);
+                    console.log(`Account: ${linkInfo.account}`);
+                } else if (response.status === 401) {
+                    console.error('Unauthorized:', response.status);
+                } else if (response.status === 409) {
+                    console.error('Conflict: A link with that slug/destination already exists');
+                } else if (response.status === 422) {
+                    console.error('Unprocessable Entity: Destination doesn\'t match URL regex');
+                } else {
+                    console.error('Failed to create the link:', response.status);
+                }
+            } catch (error) {
+                console.error('An error occurred:', error.message);
+            }
+        } else {
+            console.error('Invalid arguments. Please provide the slug, destination, and author.');
+        }
+    });
+
+// Update a link
+program
+    .command('update <id> <slug> <destination>')
+    .description('Update an existing link')
+    .requiredOption('-a, --author <author>', 'Author ID')
+    .action(async (id, slug, destination, options) => {
+        if (id && slug && destination) {
+            const apiUrl = config.apiUrl; // Assuming you have already configured the API URL
+            const secret = config.apiKey; // Assuming you have the API key in config.apiKey
+
+            const headers = {
+                'secret': secret, // Set the header name for authorization
+                'Authorization': config.apiKey, // Set the API Key from the configuration file
+            };
+
+            const data = {
+                id,
+                slug,
+                destination,
+                author: options.author,
+            };
+
+            try {
+                const response = await axios.patch(`${apiUrl}/link`, data, { headers });
+                if (response.status === 200) {
+                    const linkInfo = response.data.result;
+                    console.log('Link Successfully Updated:');
+                    console.log(`ID: ${linkInfo.id}`);
+                    console.log(`Slug: ${linkInfo.slug}`);
+                    console.log(`Destination: ${linkInfo.destination}`);
+                    console.log(`Author: ${linkInfo.author}`);
+                    console.log(`Creation Date: ${linkInfo.creationDate}`);
+                    console.log(`Modified Date: ${linkInfo.modifiedDate}`);
+                    console.log(`Visits: ${linkInfo.visits}`);
+                    console.log(`Account: ${linkInfo.account}`);
+                } else if (response.status === 401) {
+                    console.error('Unauthorized:', response.status);
+                } else if (response.status === 403) {
+                    console.error('Forbidden: You do not have permissions to edit this link');
+                } else if (response.status === 404) {
+                    console.error('Not Found: A link with that id does not exist');
+                } else if (response.status === 409) {
+                    console.error('Conflict: A link with that slug/destination already exists');
+                } else if (response.status === 422) {
+                    console.error('Unprocessable Entity: Invalid slug/destination format');
+                } else {
+                    console.error('Failed to update the link:', response.status);
+                }
+            } catch (error) {
+                console.error('An error occurred:', error.message);
+            }
+        } else {
+            console.error('Invalid arguments. Please provide the link ID, slug, destination, and author.');
+        }
+    });
+
+// Delete a link
+program
+    .command('delete <ids...>')
+    .description('Delete one or more links')
+    .requiredOption('-a, --author <author>', 'Author ID')
+    .action(async (ids, options) => {
+        if (ids.length > 0) {
+            const apiUrl = config.apiUrl; // Assuming you have already configured the API URL
+            const secret = config.apiKey; // Assuming you have the API key in config.apiKey
+
+            const headers = {
+                'secret': secret, // Set the header name for authorization
+                'Authorization': config.apiKey, // Set the API Key from the configuration file
+            };
+
+            const data = {
+                ids,
+                author: options.author,
+            };
+
+            try {
+                const response = await axios.delete(`${apiUrl}/link`, { data, headers });
+                if (response.status === 200) {
+                    console.log('Link(s) Successfully Deleted:');
+                    console.log(response.data.result);
+                } else if (response.status === 401) {
+                    console.error('Unauthorized:', response.status);
+                } else if (response.status === 403) {
+                    console.error('Forbidden: You do not have the permissions to delete some or all of the selected links. No links were deleted.');
+                } else {
+                    console.error('Failed to delete the link(s):', response.status);
+                }
+            } catch (error) {
+                console.error('An error occurred:', error.message);
+            }
+        } else {
+            console.error('Invalid arguments. Please provide at least one link ID to delete.');
+        }
+    });
+
+/*
+|----------------------------------------------------------------------------------
+| a
 |----------------------------------------------------------------------------------
  */
 
